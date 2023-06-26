0a236891a5ab46e6b456c5391e94bb91f8173a93
azeon125 <aminduindushan125@gmail.com>
2023-06-26 15:03:36 +0530

    add auto generate ID part three

--- db/db.js
+++ db/db.js
@@ -4,18 +4,18 @@ import {Order} from "../model/Order.js";
 import {OrderDetail} from "../model/OrderDetail.js";
 
 var customerList=[];
-customerList.push(new Customer("C001","Kamal","Mathara",55250.00));
-customerList.push(new Customer("C002","Niamal","Galle",852250.00));
-customerList.push(new Customer("C003","Danapala","Colombo",921250.00));
-customerList.push(new Customer("C004","Danapala","Gampaha",121250.00));
-customerList.push(new Customer("C005","Gunapala","Kurunagale",15250.00));
+customerList.push(new Customer("C001","Sumudu","Tangalle",75000.00));
+customerList.push(new Customer("C002","Saman","Panadura",85000.00));
+customerList.push(new Customer("C003","Ashen","Anuradapura",25000.00));
+customerList.push(new Customer("C004","Sudath","Mathara",95000.00));
+customerList.push(new Customer("C005","Kasun","Galle",72000.00));
 
 var itemList=[];
-itemList.push(new Item("P001","Keyboard",10,500.00));
-itemList.push(new Item("P002","Mouse",15,200.00));
-itemList.push(new Item("P003","Monitor",5,10000.00));
-itemList.push(new Item("P004","Pen Drive",20,1000.00));
-itemList.push(new Item("P005","Hard disk",15,5000.00));
+itemList.push(new Item("P001","Headset",25,800.00));
+itemList.push(new Item("P002","Monitor",10,12000.00));
+itemList.push(new Item("P003","Usb Hub",50,600.00));
+itemList.push(new Item("P004","Cooling Pad",30,1800.00));
+itemList.push(new Item("P005","Power Supply",35,2800.00));
 
 
 var orderList=[];
